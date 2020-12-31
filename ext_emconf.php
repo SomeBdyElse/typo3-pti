9b64267b3b20703b2bb5461bfbb22b89e99c211e
Hannes Lau <office@hanneslau.de>
2020-12-31 17:47:22 +0100

    [TASK] Drop support for TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,4 +2,10 @@
 $EM_CONF['pti'] = [
     'title' => 'PTI',
     'description' => 'Prototype Integration - Core',
+    'version' => '2.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '*',
+        ],
+    ],
 ];
