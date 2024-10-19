8fa23a434c1639a31d15eb43e535693d433d3453
Hannes Lau <office@hanneslau.de>
2024-10-19 23:59:33 +0200

    [TASK] Remove unrelated backend form validator

--- ext_localconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('TYPO3') or die();
-
-(function () {
-    // Register TCA validator
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'] = ['PrototypeIntegration\PrototypeIntegration\Evaluator\PhoneNumberValidation' => ''];
-})();
